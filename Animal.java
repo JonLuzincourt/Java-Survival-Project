b5576632f52f7996fcb2bf771264d9bee734d17b
JonLuzincourt <36981034+JonLuzincourt@users.noreply.github.com>
2018-12-15 18:18:02 -0500

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,13 @@
+class Animal{
+    //temporary location
+    //hunter
+    //hunted
+    //health
+    //size
+    //move
+    public class Herbivore extends Animal{
+        
+    }
+    public class Carnivore extends Carnivore{
+                
+    }
