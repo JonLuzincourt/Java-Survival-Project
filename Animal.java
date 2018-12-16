387d6da4d44eb016fae84670ed6525bd767812f2
JonLuzincourt <36981034+JonLuzincourt@users.noreply.github.com>
2018-12-15 19:11:28 -0500

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -2,12 +2,11 @@ class Animal{
     //temporary location
     //hunter
     //hunted
-    //health
-    //size
+    int damage = 2;
     //move
     public class Herbivore extends Animal{
-        
+        int maxHerbHealth = 25;
     }
     public class Carnivore extends Carnivore{
-                
+        int maxCarnHealth = 75;       
     }
